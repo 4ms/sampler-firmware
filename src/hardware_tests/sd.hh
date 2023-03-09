83c6dd286a8eb78ae6c179324ec9f608cab329a0
Dan Green <danngreen1@gmail.com>
2023-03-08 22:27:03 -0800

    add SDCard tests

--- src/hardware_tests/sd.hh
+++ src/hardware_tests/sd.hh
@@ -2,15 +2,66 @@
 #include "conf/board_conf.hh"
 #include "conf/sd_conf.hh"
 #include "drivers/sdcard.hh"
+#include "fatfs/fat_file_io.hh"
+#include "fatfs/sdcard_ops.hh"
 #include "hardware_tests/util.hh"
+#include "util/zip.hh"
 
 namespace SamplerKit::HWTests
 {
 
 struct TestSDCard {
-	mdrivlib::SDCard<Brain::SDCardConf> sd;
+	SDCardOps<Brain::SDCardConf> sdcard_ops;
+	FatFileIO sdcard{&sdcard_ops, Volume::SDCard};
+
+	void run_fatfs_test() {
+		std::string_view wbuf{"This is a \ntest of writing\n to a file."};
+		bool ok = false;
+
+		while (!ok) {
+			Board::RevLED{}.set_color(Colors::off);
+			Board::PlayLED{}.set_color(Colors::off);
+			Board::BankLED{}.set_color(Colors::off);
+
+			Board::RevG::set(true);
+			ok = sdcard.create_file("test.txt", wbuf);
+			if (!ok) {
+				Board::RevR::set(true);
+				Util::flash_mainbut_until_pressed();
+				continue;
+			}
+
+			std::array<char, 128> rbuf;
+			Board::BankG::set(true);
+			auto bytes_read = sdcard.read_file("test.txt", rbuf);
+			if (!bytes_read || bytes_read != wbuf.size()) {
+				ok = false;
+				Board::BankR::set(true);
+				Util::flash_mainbut_until_pressed();
+				continue;
+			}
+
+			for (auto [w, r] : zip(wbuf, rbuf)) {
+				if (w != r) {
+					ok = false;
+					Board::RevB::set(true);
+					Board::BankR::set(true);
+					Util::flash_mainbut_until_pressed();
+					continue;
+				}
+			}
+		}
+
+		// sdcard.foreach_file_with_ext(".wav", [&](std::string_view fname, uint32_t timestamp, uint32_t fsize) {
+		// 	printf_("%.64s %08x %dB\n", fname.data(), timestamp, fsize);
+		// 	// action(fno.fname, timestamp, fno.fsize);
+		// });
+	}
 
 	void run_test() {
+		auto &sd = sdcard_ops.sd;
+		// mdrivlib::SDCard<Brain::SDCardConf> sd;
+
 		std::array<uint8_t, 512> buf;
 		for (auto &x : buf)
 			x = 0xAA;

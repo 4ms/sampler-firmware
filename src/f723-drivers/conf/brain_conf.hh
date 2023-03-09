e1fc1c0692d0d81dc092c386951fcf10ea1f9a11
Dan Green <danngreen1@gmail.com>
2023-03-09 06:44:59 -0800

    WIP Console

--- src/f723-drivers/conf/brain_conf.hh
+++ src/f723-drivers/conf/brain_conf.hh
@@ -85,23 +85,22 @@ struct Disabled {
 	static void high() {}
 	static void low() {}
 };
-// using Pin0 = Disabled;
-// using Pin1 = Disabled;
-// using Pin2 = Disabled;
-// using Pin3 = Disabled;
-using Pin0 = mdrivlib::FPin<mdrivlib::GPIO::H, mdrivlib::PinNum::_13>;
-using Pin1 = mdrivlib::FPin<mdrivlib::GPIO::H, mdrivlib::PinNum::_14>;
+using Pin0 = Disabled;
+using Pin1 = Disabled;
+
+// using Pin0 = mdrivlib::FPin<mdrivlib::GPIO::H, mdrivlib::PinNum::_13>;
+// using Pin1 = mdrivlib::FPin<mdrivlib::GPIO::H, mdrivlib::PinNum::_14>;
 using Pin2 = mdrivlib::FPin<mdrivlib::GPIO::H, mdrivlib::PinNum::_15>;
 using Pin3 = mdrivlib::FPin<mdrivlib::GPIO::D, mdrivlib::PinNum::_3>;
 }; // namespace Debug
 
-struct DebugConsole {
+struct Console {
 	mdrivlib::Uart<UART4_BASE> console;
 	void console_init() {
-		// mdrivlib::Pin uart_tx{
-		// 	Debug::Pin0{}.Gpio_v, Debug::Pin0{}.PinNum_v, mdrivlib::PinMode::Alt, mdrivlib::PinAF::AltFunc8};
-		// mdrivlib::Pin uart_rx{
-		// 	Debug::Pin1{}.Gpio_v, Debug::Pin1{}.PinNum_v, mdrivlib::PinMode::Alt, mdrivlib::PinAF::AltFunc8};
+		mdrivlib::Pin uart_tx{
+			mdrivlib::GPIO::H, mdrivlib::PinNum::_13, mdrivlib::PinMode::Alt, mdrivlib::PinAF::AltFunc8};
+		mdrivlib::Pin uart_rx{
+			mdrivlib::GPIO::H, mdrivlib::PinNum::_14, mdrivlib::PinMode::Alt, mdrivlib::PinAF::AltFunc8};
 	}
 };
 } // namespace Brain

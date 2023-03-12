cfc35ec73635ca6a46ad7bda9588b090851ec8c3
Dan Green <danngreen1@gmail.com>
2023-03-12 14:54:29 -0700

    Temp fix to use Rev button not Play (botched LED fix)

--- src/hardware_tests/util.hh
+++ src/hardware_tests/util.hh
@@ -6,7 +6,10 @@
 namespace SamplerKit::HWTests
 {
 struct UtilIF {
-	static bool main_button_pressed() { return Board::PlayButton::PinT::read(); }
+	static bool main_button_pressed() {
+		// return Board::PlayButton::PinT::read();
+		return Board::RevButton::PinT::read();
+	}
 	static void delay_ms(uint32_t ms) { HAL_Delay(ms); }
 	static void set_main_button_led(bool turn_on) { Board::PlayG::set(turn_on); }
 };

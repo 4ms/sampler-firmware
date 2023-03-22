22295ac7013d914ed6062efcd6ff274822b2ae39
Dan Green <danngreen1@gmail.com>
2023-03-22 16:55:22 -0700

    Add busy light for SD card (Rev Red)

--- src/fatfs/sdcard_ops.hh
+++ src/fatfs/sdcard_ops.hh
@@ -3,6 +3,11 @@
 #include "disk_ops.hh"
 #include "drivers/sdcard.hh"
 
+// Hack a busy light into this.
+// FixME: make this part of SdCardConf: a busy indicator?
+#include "conf/board_conf.hh"
+using SdCardBusyLED = SamplerKit::Board::RevR;
+
 template<mdrivlib::SDCardConfC ConfT>
 class SDCardOps : public DiskOps {
 	constexpr static uint32_t SDBlockSize = 512;
@@ -14,6 +19,9 @@ public:
 
 	SDCardOps(SDCardOps &other) = delete;
 	SDCardOps() = default;
+
+	void busy_light_on() { SdCardBusyLED::high(); }
+	void busy_light_off() { SdCardBusyLED::low(); }
 
 	DSTATUS status() override { return (_status == Status::NotInit) ? (STA_NOINIT | STA_NODISK) : 0; }
 
@@ -44,6 +52,7 @@ public:
 	}
 
 	DRESULT read(uint8_t *dst, uint32_t sector_start, uint32_t num_sectors) override {
+		busy_light_on();
 		if (!sd.detect_card()) {
 			_status = Status::NoCard;
 			return RES_NOTRDY;
@@ -51,10 +60,12 @@ public:
 
 		const uint32_t size_bytes = num_sectors * sd.BlockSize;
 		auto ok = sd.read({dst, size_bytes}, sector_start);
+		busy_light_off();
 		return ok ? RES_OK : RES_ERROR;
 	}
 
 	DRESULT write(const uint8_t *src, uint32_t sector_start, uint32_t num_sectors) override {
+		busy_light_on();
 		if (!sd.detect_card()) {
 			_status = Status::NoCard;
 			return RES_NOTRDY;
@@ -62,6 +73,7 @@ public:
 
 		const uint32_t size_bytes = num_sectors * sd.BlockSize;
 		auto ok = sd.write({src, size_bytes}, sector_start);
+		busy_light_off();
 		return ok ? RES_OK : RES_ERROR;
 	}
 
@@ -79,15 +91,19 @@ public:
 				break;
 
 			case GET_SECTOR_COUNT: {
+				busy_light_on();
 				uint32_t num_blocks = sd.get_card_num_blocks();
 				if (!num_blocks)
 					return RES_NOTRDY;
 				*(DWORD *)buff = num_blocks;
+				busy_light_off();
 			} break;
 
 			case MMC_GET_SDSTAT: {
+				busy_light_on();
 				uint8_t mounted = sd.detect_card();
 				*(uint8_t *)buff = mounted;
+				busy_light_off();
 			} break;
 
 			default:

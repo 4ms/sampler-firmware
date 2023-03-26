3c19b758875d7f035cad02c5b357328338239613
Dan Green <danngreen1@gmail.com>
2023-03-26 11:33:44 -0700

    Enable I-Cache for F723

--- src/f723-drivers/system_target.hh
+++ src/f723-drivers/system_target.hh
@@ -10,7 +10,15 @@ struct SystemTarget {
 	static void init() {
 		mdrivlib::SDRAMPeriph sdram{Brain::SDRAM_conf, Brain::SdramBank, Brain::SdramKernelClock};
 		SCB_InvalidateICache();
-		SCB_DisableICache();
+		SCB_EnableICache();
+
+		// SCB_EnableDCache();
+		// SCB_CleanInvalidateDCache();
+
+		// Data Cache not needed for this app:
+		// Even with max modulation and resampling rate, audio processing loop never exceeds 25% load
+		// Disabling DCache keeps the DMA transfers more simple
+		SCB_DisableDCache();
 	}
 };
 } // namespace SamplerKit

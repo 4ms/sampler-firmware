bfe055a40527e916e8fa52e44114edbe42ec3311
Dan Green <danngreen1@gmail.com>
2023-04-11 14:22:09 -0700

    Tweak palette

--- src/palette.hh
+++ src/palette.hh
@@ -20,16 +20,16 @@ struct SamplerColors {
 	static constexpr Color magenta = Color(200, 0, 100);
 
 	struct Bank {
-		static constexpr Color white = Color(200, 200, 200);
+		static constexpr Color white = Color(180, 220, 220);
 		static constexpr Color red = Color(255, 0, 0);
 		static constexpr Color orange = Color(255, 127, 0);
 		static constexpr Color yellow = Color(200, 255, 0);
 		static constexpr Color green = Color(0, 200, 0);
 		static constexpr Color cyan = Color(0, 160, 200);
 		static constexpr Color blue = Color(0, 0, 255);
-		static constexpr Color pink = Color(200, 0, 100);
-		static constexpr Color purple = Color(120, 0, 120);
-		static constexpr Color grey = Color(80, 140, 60);
+		static constexpr Color pink = Color(200, 0, 200);
+		static constexpr Color purple = Color(100, 50, 180);
+		static constexpr Color grey = Color(100, 140, 120);
 	};
 };
 

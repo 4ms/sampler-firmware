5fe1cb09e66b2dcdce3d0a7d40b5b5c7df6e3246
Dan Green <danngreen1@gmail.com>
2023-05-07 22:14:50 -0700

    Move Sample class to sample_type.hh

--- src/sample_file.hh
+++ src/sample_file.hh
@@ -27,51 +27,15 @@
  */
 
 #pragma once
+#include "elements.hh"
 #include "ff.h"
-#include "sdcard.hh"
+#include "sample_type.hh"
+#include <array>
 
 namespace SamplerKit
 {
-enum class FileStatus : uint8_t { NotFound = 0, Found = 1, NewFile = 2 };
 
-struct Sample {
-	char filename[FF_MAX_LFN];
-	uint32_t sampleSize;
-	uint32_t startOfData;
-	uint8_t sampleByteSize;
-	uint32_t sampleRate;
-	uint8_t numChannels;
-	uint8_t blockAlign;
-
-	uint32_t inst_start;
-	uint32_t inst_end;
-	uint32_t inst_size;
-	float inst_gain;
-
-	uint16_t PCM;
-	FileStatus file_status;
-
-	uint8_t num_cues;
-	uint32_t cue[4];
-
-	Sample() { clear(); }
-	void clear() {
-		filename[0] = 0;
-		sampleSize = 0;
-		sampleByteSize = 0;
-		sampleRate = 0;
-		numChannels = 0;
-		blockAlign = 0;
-		startOfData = 0;
-		PCM = 0;
-		file_status = FileStatus::NotFound;
-		inst_start = 0;
-		inst_end = 0;
-		inst_size = 0;
-		inst_gain = 1.0;
-		num_cues = 0;
-	}
-};
+struct Sdcard;
 
 using Bank = std::array<Sample, NumSamplesPerBank>;
 using SampleList = std::array<Bank, MaxNumBanks>;

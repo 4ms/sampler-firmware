c4c414cf10960c570baf6992c24de8338617a170
Dan Green <danngreen1@gmail.com>
2023-05-15 06:24:23 -0700

    Write settings to SD Card when saving sample layout

--- src/button_actions.hh
+++ src/button_actions.hh
@@ -108,7 +108,7 @@ struct ButtonActionHandler {
 			if (!ignore_rev_release && controls.rev_button.how_long_held_pressed() > two_sec) {
 				if (!ignore_play_release && controls.play_button.how_long_held_pressed() > two_sec) {
 					flags.set(Flag::WriteIndexToSD);
-					// flags.set(Flag::WriteSettingsToSD);
+					flags.set(Flag::WriteSettingsToSD);
 					ignore_bank_release = true;
 					ignore_play_release = true;
 					ignore_rev_release = true;

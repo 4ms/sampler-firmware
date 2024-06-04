0dabfb28878ec0127c487cf5c30e329424a25f41
Dan Green <danngreen1@gmail.com>
2024-06-04 14:11:44 -0700

    Add bootloader settings for scramble and outtro size

--- src/bootloader/f723/conf/bootloader_settings.hh
+++ src/bootloader/f723/conf/bootloader_settings.hh
@@ -4,6 +4,11 @@ namespace BootloaderConf
 static constexpr uint32_t SampleRate = 48000;
 
 constexpr inline uint32_t ReceiveSectorSize = 128 * 1024;
+constexpr inline bool DoScramble = false;
+
+// should equal blank_duration in encoder.py which sets the length of the sync tone to signal the end of the wav file
+// (not to be confused with options.blank_duration)
+constexpr inline float OutroSyncSeconds = 5.f;
 
 struct FskEncoding {
 	uint32_t blank;
--- src/bootloader/f746/conf/bootloader_settings.hh
+++ src/bootloader/f746/conf/bootloader_settings.hh
@@ -3,7 +3,14 @@ namespace BootloaderConf
 {
 static constexpr uint32_t SampleRate = 48000;
 
-constexpr inline uint32_t ReceiveSectorSize = 128 * 1024;
+// Should match --block_size argument passed to encoder.py
+constexpr inline uint32_t ReceiveSectorSize = 32 * 1024;
+
+constexpr inline bool DoScramble = true;
+
+// should equal blank_duration in encoder.py which sets the length of the sync tone to signal the end of the wav file
+// (not to be confused with options.blank_duration)
+constexpr inline float OutroSyncSeconds = 5.f;
 
 struct FskEncoding {
 	uint32_t blank;
